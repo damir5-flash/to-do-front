08fc038de2fdd6f5ea150cc3f37ea589fe53b568
damir5-flash <rayimbekdamir5@gmail.com>
2025-03-09 23:30:46 +0500

    Deploy vite to GitHub Pages

--- toDo/vite.config.js
+++ toDo/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/to-do-front/',
   plugins: [react()],
+  base: '/to-do-front/', // замените на имя вашего репо
 })
